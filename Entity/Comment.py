3d42512d6cd33b3320b26915e3ddaba784f2df4d
leffy <jxliang.left@gmail.com>
2015-01-29 16:37:54 +0800

    finish fansSpider and the hibernation of Enity

---
+++ Entity/Comment.py
@@ -0,0 +1,5 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+class Comment:
+    def __init__(self):
+        pass
---
+++ Entity/User.py
@@ -0,0 +1,6 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+class User:
+    def __init__(self):
+        pass
