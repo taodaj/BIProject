912532fed24dd519593012dd7d724d607ae10245
Moirai7 <emma@moirai.cn>
2015-01-29 18:17:50 +0800

    refine

--- Crawler/Python/WeiboElement/weiboInfo.py
+++ Crawler/Python/WeiboElement/weiboInfo.py
@@ -3,43 +3,48 @@
 # -*- coding:UTF-8 -*-
 
 import sys
-import userInfo.py
+import sys
+
+sys.path.append('..')
+import WeiboElement.userInfo
 
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
-class weiboInfo(object):
+class weiboInfo():
+
     def __init__(self,weiboid,userid,content,time,fowardingNum,commentNum,likeNum,plantform):
         self.weiboid=weiboid
         self.userid=userid
-	self.content=content
-	self.time=time
-	self.fowardingNum=fowardingNum
-	self.commentNum=commentNum
-	self.likeNum=likeNum
-	self.plantform=plantform
+        self.content=content
+        self.time=time
+        self.fowardingNum=fowardingNum
+        self.commentNum=commentNum
+        self.likeNum=likeNum
+        self.plantform=plantform
     def getWeiboid():
-	return self.weiboid
+        return self.weiboid
     def getUserid():
-	return self.userid
+        return self.userid
     def getContent():
-	return self.content
+        return self.content
     def getTime():
-	return self.time
+        return self.time
     def getFowardingNum():
-	return self.fowardingNum
+        return self.fowardingNum
     def getCommentNum():
-	return self.commentNum
+        return self.commentNum
     def getLikeNum():
-	return self.likeNum
+        return self.likeNum
     def getPlantform():
-	return self.plantform
-    def setAT(object):
-	self.atJson=atJson
+        return self.plantform
+    def setAT(self,object):
+        self.atJson=object
     def getAT():
-	return self.atJson
-    def setfowardingWeiboId(object):
+        return self.atJson
+    def setfowardingWeiboId(self,object):
         self.fowardingWeiboId=object
     def getfowardingWeiboId():
-	return self.fowardingWeiboId
+        return self.fowardingWeiboId
+print "asdkfjaksdf"
     
--- Crawler/Python/WeiboUtil/weiboResolution.py
+++ Crawler/Python/WeiboUtil/weiboResolution.py
@@ -1,4 +1,5 @@
-#!/bin/python
+﻿#!/bin/python
+# FileName : weiboResolution.py
 # -*- coding: utf-8 -*-
 
 import cookielib
@@ -9,130 +10,149 @@ import logging
 import time
 import random
 import sys
+from lxml import etree
 
-logging.basicConfig(level=logging.DEBUG,  
-                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',  
-                    datefmt='%a, %d %b %Y %H:%M:%S',  
-                    stream=sys.stdout  
-                    ) 
+sys.path.append('..')
+import WeiboElement.weiboInfo as weiboInfo
+#import weiboInfo
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',datefmt='%a, %d %b %Y %H:%M:%S',stream=sys.stdout) 
 
 
 class BlockedException(Exception):
-	def __init__(self):
-		Exception.__init__(self)
+    def __init__(self):
+        Exception.__init__(self)
 
-class weiboSpider:
-	def __init__(self,username,password):
-		self.username=username
-		self.password=password
-		try: 
-			#login and build opener with cookie
-			self.opener=self.login()
-		except urllib2.HTTPError as e:
-			print e
-			exit()
-		except Exception as e:
-			print e
-			exit()
+class weiboResolution:
+    def __init__(self,username,password):
+        self.username=username
+        self.password=password
+        
 
-	def login(self):
-		#create cookie
-		cookieJ = cookielib.CookieJar()
-		#install cookie
-	  	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJ))
-		#pretend to be a browser
-		opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36')]
-		#open login page
-        	response=opener.open('http://login.weibo.cn/login/')
-        	loginText=response.read()
-       		Pyquery=pyquery.PyQuery(loginText)
-		#get login url
-       		loginURL=Pyquery('form').attr('action')
-       		loginURL='http://login.weibo.cn/login/'+loginURL
-		#get password key	
-	       	password=Pyquery('input').eq(1)
-	       	password=pyquery.PyQuery(password).attr('name')
-		#get vk value
-	        vk=Pyquery('input').eq(6).val()
-		#wrap post data	
-		data={'mobile':self.username,password:self.password,'remember':'on','vk':vk,'submit':'登录'}	
-		#encode post data
-	       	data=urllib.urlencode(data)
-		#login
-		response=opener.open(loginURL,data)
-       		#<DEBUG>print response.geturl()
-		if 'http://login.weibo.cn/' in response.geturl() :
-			raise Exception("username or password is wrong")
-		logging.info("user :"+self.username+" login sucesses")
-		return opener
+    def login(self):
+        #create cookie
+        cookieJ = cookielib.CookieJar()
+        #install cookie
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJ))
+        #pretend to be a browser
+        opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36')]
+        #open login page
+        response=opener.open('http://login.weibo.cn/login/')
+        loginText=response.read()
+        Pyquery=pyquery.PyQuery(loginText)
+        #get login url
+        loginURL=Pyquery('form').attr('action')
+        loginURL='http://login.weibo.cn/login/'+loginURL
+        tree=etree.HTML(loginText)
+        #get password key	
+        password=tree.xpath('//input[@type="password"]/@name')[0]
+        #get vk value
+        vk=tree.xpath('//input[@name="vk"]/@value')[0]
+        #wrap post data	
+        data={'mobile':self.username,password:self.password,'remember':'on','vk':vk,'submit':'登录'}	
+        #encode post data
+        data=urllib.urlencode(data)
+        #login
+        response=opener.open(loginURL,data)
+        #<DEBUG>print response.geturl()
+        if 'http://login.weibo.cn/' in response.geturl() :
+            raise Exception("username or password is wrong")
+            logging.info("user :"+self.username+" login sucesses")
+        return opener
 
-	#get the users he follows
-	def getfollowing(self,userid):
-		#list to store the people he follows
-		followingList=[]
-		
-		#HTTPError may be raised
-		req=self.opener.open('http://weibo.cn/'+userid+'/follow',timeout=5)
-		# if blocked by sina, raise exception
-		self.blockedCheck('weibo.cn/'+userid+'/follow',req.geturl())
-		content=req.read()
-		followingList.extend(self.extractFollowing(content))	
+    #get the users' weibo
+    def getWeibo(self,userid):
+        #weibo list
+        weiboList=[]
+        #HTTPError may be raised
+        req=self.opener.open('http://weibo.cn/'+userid+'?page=0',timeout=5)
+        # if blocked by sina, raise exception
+        self.blockedCheck('weibo.cn/'+userid+'?page='+weiboid,req.geturl())
+        content=req.read()
+        #extract data
+        followingList.extend(self.extractFollowing(content,userid))
+        #rest for a while
+        restTime=self.randomRest()
+        #logging
+        logging.info('rested for '+str(restTime)+' collect from '+req.geturl())
+        #extract next url
+        while True:
+            nextUrl=self.extractFollowingUrl(content)
+            if nextUrl == None:
+                break
+            req=self.opener.open('http://weibo.cn'+nextUrl,timeout=5)
+            self.blockedCheck('weibo.cn/'+userid+'/follow',req.geturl())
+            try:
+                content=req.read()
+                #extract data
+                weiboList.extend(self.extractFollowing(content,userid))
+                #rest for a while
+                restTime=self.randomRest()
+                #logging
+                logging.info('rested for '+str(restTime)+' collect from '+req.geturl())
+            except socket.timeout as e:
+                #logging
+                logging.info('TIME OUT, try again from '+req.geturl())
+                continue 
 
-		#rest for a while
-		restTime=self.randomRest()
-		#logging
-		logging.info('rested for '+str(restTime)+' collect from '+req.geturl())
-		#extract next url
-		while True:
-			nextUrl=self.extractFollowingUrl(content)
-			if nextUrl == None:
-				break
-			req=self.opener.open('http://weibo.cn'+nextUrl,timeout=5)
-			self.blockedCheck('weibo.cn/'+userid+'/follow',req.geturl())
-			content=req.read()
-			#extract data
-			followingList.extend(self.extractFollowing(content))
-			#rest for a while
-			restTime=self.randomRest()
-			#logging
-			logging.info('rested for '+str(restTime)+' collect from '+req.geturl())
+        return weiboList
 
-		return followingList
+    def blockedCheck(self,expectedURL,actualURL):
+        if expectedURL not in actualURL:
+            logging.info('user : '+self.username+' is blocked by Sina')
+            raise BlockedException()
+    
+    def randomRest(self):
+        restTime=random.random()*2+5
+        time.sleep(restTime)
+        return restTime    
+    
+    def extractFollowingUrl(self,content):
+        html=pyquery.PyQuery(content)
+        for ele in html('form a'):
+            if pyquery.PyQuery(ele).text()==u'下页':
+                return pyquery.PyQuery(ele).attr('href')
+        return None
 
-	def blockedCheck(self,expectedURL,actualURL):
-		if expectedURL not in actualURL:
-			logging.info('user : '+self.username+' is blocked by Sina')
-			raise BlockedException()
-	
-	def randomRest(self):
-		restTime=random.random()*2+5
-		time.sleep(restTime)
-		return restTime		
-	
-	def extractFollowingUrl(self,content):
-		html=pyquery.PyQuery(content)
-		for ele in html('form a'):
-			if pyquery.PyQuery(ele).text()==u'下页':
-				return pyquery.PyQuery(ele).attr('href')
-		return None
-
-	#ATTENTION: Here assume spider follows nobody
-	def extractFollowing(self,content):
-		pageList=[]
-		html=pyquery.PyQuery(content)
-		for ele in html('table'):
-			try:
-				name=pyquery.PyQuery(ele)('td').eq(1)('a').eq(0).text()
-				uid=pyquery.PyQuery(ele)('td').eq(1)('a').eq(1).attr('href').split('?')[1].split('&')[0].split('=')[1]
-				fans=pyquery.PyQuery(ele)('td').text().split(' ')[1][2:-1]
-				pageList.append(name+','+uid+','+fans)
-			except AttributeError as e:
-				print e 
-				continue
-		return pageList
+    #ATTENTION: Here assume spider follows nobody
+    def extractFollowing(self,content,userid):
+        atsList=[]
+        tree=etree.HTML(content)
+        #tree=etree.HTML(content)
+        i=0
+        for ele in tree.xpath(u"//div[@class='c' and @id]"):
+            weiboid=ele.get("id").split('_')[1]
+            hrefs = ele.xpath(u"div/span[@class='cmt']/a")
+            if hrefs:
+                fowardingWeiboId=hrefs[0].text
+                content=ele.xpath(u"div[last()]/text()")[0]
+                ats=ele.xpath(u"div[last()]/a")[0]
+            else :
+                fowardingWeiboId=" "
+                content=ele.xpath(u"div/span[@class='ctt']")[0].text
+                ats=ele.xpath(u"div/span[@class='ctt']/a")
+            for at in ats:
+                strat=at.get('href')
+                if strat.find('/n/')!=-1:
+                    atsList.append(strat[2:-1])  
+            likeNum=ele.xpath(u"div[last()]/a[last()-3]")[0].text
+            commentNum=ele.xpath(u"div[last()]/a[last()-1]")[0].text
+            fowardingNum=ele.xpath(u"div[last()]/a[last()-2]")[0].text
+            info=ele.xpath(u"div[last()]/span[@class='ct']")[0].text.split(u'来自')
+            time=info[0]
+            plantform=info[1]
+            weibo=weiboInfo.weiboInfo(weiboid,userid,content,time,fowardingNum,commentNum,likeNum,plantform)
+            if hrefs:
+                weibo.setfowardingWeiboId(fowardingWeiboId)
+            if len(atsList):
+                weibo.setAT(atsList)
+            print weiboid+","+fowardingWeiboId+","+likeNum+","+commentNum+","+time+","+plantform+","+fowardingNum 
+        return weibo
 
 if __name__ == '__main__':
-	spider = weiboSpider('18612986170', '18612986170')
-	print spider.getfollowing('3399558022')
-
+    spider = weiboResolution('18612986170', '18612986170')
+    #print spider.extractFollowing('3399558022')
+    spider.extractFollowing(html,'3399558022')
