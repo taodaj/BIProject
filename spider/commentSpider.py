47dba92c639d4b5f5466be4c9a98a7ca265017ff
chuncaohenli <604265882@qq.com>
2015-02-05 18:07:03 +0800

    Update commentSpider.py
    userid->uid,weiboid->wid,新加cid

--- spider/commentSpider.py
+++ spider/commentSpider.py
@@ -90,7 +90,7 @@ class commentSpider(spider):
                 time_sent = self.timeFormat(timearray)
 
                 print agree,time_sent,comment
-                commentContent = {'userid':self.userid,'weiboid':self.weiboid,'comment':comment,'agree':agree,'time':time_sent}
+                commentContent = {'uid':self.userid,'wid':self.weiboid,'comment':comment,'agree':agree,'time':time_sent,'cid':id}
                 commentList.append(commentContent)
 
         return commentList
