e399c2e65c919650e5d78f96d957337de0a898a8
chuncaohenli <604265882@qq.com>
2015-02-05 22:20:43 +0800

    Update CommentSpider.py
    修改时间转换函数

--- Spider/CommentSpider.py
+++ Spider/CommentSpider.py
@@ -17,7 +17,8 @@ try:
 except ImportError:
     import xml.etree.ElementTree as ET
 
-from Spider import *
+from spider import spider
+from spider import BlockedException
 
 
 
@@ -30,10 +31,10 @@ logging.basicConfig(level=logging.DEBUG,
 
 
 
-class CommentSpider(Spider):
+class commentSpider(spider):
 
     def __init__(self,username,password):
-        Spider.__init__(self,username,password)
+        spider.__init__(self,username,password)
 
     # get weibo comment
     def getComment(self, userid,weiboid):
@@ -73,13 +74,13 @@ class CommentSpider(Spider):
         commentList = []
         idList = tree.xpath('//div/@id')
 
-        for cid in idList:
-            if(re.match('C_',cid)):
-                xpath_comment='//div[@id="'+cid+'"]'+'/span[@class="ctt"]'
-                xpath_comment_text='//div[@id="'+cid+'"]'+'/span[@class="ctt"]/text()'
-                xpath_comment_at = '//div[@id="'+cid+'"]'+'/span[@class="ctt"]/a/text()'
-                xpath_time='//div[@id="'+cid+'"]'+'/span[@class="ct"]/text()'
-                xpath_agree='//div[@id="'+cid+'"]'+'/span[@class="cc"][1]/a/text()'
+        for id in idList:
+            if(re.match('C_',id)):
+                xpath_comment='//div[@id="'+id+'"]'+'/span[@class="ctt"]'
+                xpath_comment_text='//div[@id="'+id+'"]'+'/span[@class="ctt"]/text()'
+                xpath_comment_at = '//div[@id="'+id+'"]'+'/span[@class="ctt"]/a/text()'
+                xpath_time='//div[@id="'+id+'"]'+'/span[@class="ct"]/text()'
+                xpath_agree='//div[@id="'+id+'"]'+'/span[@class="cc"][1]/a/text()'
                 #comment 评论内容
                 comment = tree.xpath('string('+xpath_comment+')')
                 #agree 赞同数
@@ -89,7 +90,7 @@ class CommentSpider(Spider):
                 time_sent = self.timeFormat(timearray)
 
                 print agree,time_sent,comment
-                commentContent = {'uid':self.userid,'wid':self.weiboid,'comment':comment,'agree':agree,'time':time_sent,'cid':cid}
+                commentContent = {'userid':self.userid,'weiboid':self.weiboid,'comment':comment,'agree':agree,'time':time_sent}
                 commentList.append(commentContent)
 
         return commentList
@@ -103,14 +104,12 @@ class CommentSpider(Spider):
         # xx分钟前
         elif(len(timearray)==1):
             minute_ago = int(timearray[0].split('分钟前')[0])
-            minute_now = int(time.strftime('%M',time.localtime(time.time())))
-            date_minute_now = datetime.datetime.fromtimestamp(minute_now)
-            date_time_sent = date_minute_now - datetime.timedelta(minutes=minute_ago)
-            print date_time_sent
+            date_now = datetime.datetime.fromtimestamp(time.time())
+            date_time_sent = date_now - datetime.timedelta(minutes=minute_ago)
             time_sent = datetime.datetime.strftime(date_time_sent,'%m月%d日 %H时%M分')
         return unicode(time_sent,'utf8')
 
 if __name__ == '__main__':
     #!!!!USE YOUR USERNAME AND PASSWORD HERE
-    spider = commentSpider(USERNAME, PASSWORD)
-    print spider.getComment('1644088832','C1JhOhrwM')
+    spider = commentSpider('chuncaohenli@gmail.com', 'gxx01111110')
+    print spider.getComment('1341902602','C2VwGgjjr')
