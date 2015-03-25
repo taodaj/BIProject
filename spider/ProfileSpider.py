3ed0d30222222e7c31c21234b7da08c275315840
12301121-金林荣 <564257051@qq.com>
2015-03-25 12:04:16 +0800

    Update ProfileSpider.py

--- spider/ProfileSpider.py
+++ spider/ProfileSpider.py
@@ -99,7 +99,7 @@ class ProfileSpider(Spider):
         #如果未得到对应信息（即被反爬虫），重新获取。若5次都失败，则放弃。
         for i in range(0,5,1):
             try:
-                print 'uid:',uid  
+                logging.debug('uid:'+uid)  
                 #获取对应网址的content
                 content=self.downloadHTML(url,expectedURLSeg)  
                 #提取微博数、粉丝数、关注人数
@@ -137,15 +137,15 @@ class ProfileSpider(Spider):
             #提取微博数
             weiboNum=tree.xpath('/html/body/div['+str(self.placeNum)+']/div/span/text()')[0][3:-1]
             self.obj['weiboNum']=weiboNum
-            print '微博数:',weiboNum
+            logging.debug('微博数:'+weiboNum)
             #提取关注数
             followingNum=tree.xpath('/html/body/div['+str(self.placeNum)+']/div/a[1]/text()')[0][3:-1]
             self.obj['followingNum']=followingNum
-            print '关注数:',followingNum
+            logging.debug('关注数:'+followingNum)
             #提取粉丝数
             fansNum=tree.xpath('/html/body/div['+str(self.placeNum)+']/div/a[2]/text()')[0][3:-1]
             self.obj['fansNum']=fansNum
-            print '粉丝数:',fansNum
+            logging.debug('粉丝数:'+fansNum)
         except AttributeError as e:
             print e 
 
@@ -153,16 +153,12 @@ class ProfileSpider(Spider):
     def extractInfo(self,content):
         #将html内容变成xpath能处理的tree
         tree=etree.HTML(content)
-        #提取微博等级
-        level=tree.xpath('/html/body/div['+str(self.placeNum+1)+']/a[1]/text()')[0][:-1]
-        self.obj['level']=level
-        print '微博等级:',level
-        #提取会员等级
-        vipLevel=tree.xpath('/html/body/div['+str(self.placeNum+1)+']/text()')[1][5:-2]
+        #提取微博vip等级
+        vipLevel=tree.xpath('/html/body/div['+str(self.placeNum+1)+']/text()')[0][5:]
         if ('未开' in vipLevel): 
             vipLevel='null'
         self.obj['vipLevel']=vipLevel
-        print '会员等级:',vipLevel
+        logging.debug('会员等级:'+vipLevel)
         #初始化变量
         name='null'
         location='null'
@@ -177,6 +173,7 @@ class ProfileSpider(Spider):
         infoArray=tree.xpath('/html/body/div['+str(self.placeNum+3)+']/text()')
         #对信息块遍历，提取相应信息
         for ele in infoArray:
+            label='null'
             label=ele[0:2]            
             if cmp(label,'昵称') ==0:    
                 name=ele[3:]
@@ -197,19 +194,19 @@ class ProfileSpider(Spider):
                     if ('更多' in ele1): 
                         continue
                     labels.append(ele1)
-                    print 'label:',ele1
+                    logging.debug('label:'+ele1)
         self.obj['name']=name
-        print 'name:',name
+        logging.debug('name:'+name)
         self.obj['gender']=gender
-        print 'gender:',gender
+        logging.debug('gender:'+gender)
         self.obj['location']=location
-        print 'location:',location
+        logging.debug('location:'+location)
         self.obj['birthday']=birthday
-        print 'birthday:',birthday
+        logging.debug('birthday:'+birthday)
         self.obj['intro']=intro
-        print 'intro:',intro
+        logging.debug('intro:'+intro)
         self.obj['identifyContent']=identifyContent
-        print 'identifyContent:',identifyContent
+        logging.debug('identifyContent:'+identifyContent)
         self.obj['label']=label
         #获取工作经历、学习经历的提示信息
         inferInfo=tree.xpath('/html/body/div['+str(self.placeNum+4)+']/text()')
@@ -220,7 +217,7 @@ class ProfileSpider(Spider):
             #对学习信息块进行遍历，提取每一条学习经历信息
             for ele in eduInfo:
                 self.obj['education']=ele
-                print 'education:',ele
+                logging.debug('education:'+ele)
             #获取工作经历的提示信息
             inferInfo=tree.xpath('/html/body/div['+str(self.placeNum+6)+']/text()')
             #如果该提示信息是工作经历，则提取工作经历(这里是在有学习经历的情况下进行提取)
@@ -232,7 +229,7 @@ class ProfileSpider(Spider):
                     pro=HTMLParser.HTMLParser().unescape(ele)
                     job.append(ele)
                     self.obj['job:']=ele
-                    print 'job:',ele
+                    logging.debug('job:'+ele)
         #获取工作经历的提示信息
         inferInfo=tree.xpath('/html/body/div['+str(self.placeNum+4)+']/text()')
         #如果该提示信息是工作经历，则提取工作经历(这里是没有学习经历的情况下进行提取)
@@ -244,7 +241,7 @@ class ProfileSpider(Spider):
                 pro=HTMLParser.HTMLParser().unescape(ele)
                 job.append(ele)
                 self.obj['job:']=ele
-                print 'job:',ele
+                logging.debug('job:'+ele)
         self.obj['education']=education
         self.obj['job']=job         
 
@@ -252,4 +249,3 @@ class ProfileSpider(Spider):
         
 
     
-
